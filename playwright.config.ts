9b52bbdf1808d33dda3d093dbcf5ffebcc2194d8
Jerishwin Joseph <jerishwinj@gmail.com>
2026-06-24 21:11:05 +0530

    24-6-26

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-  ['html'],
-  ['junit', { outputFile: 'test-results/results.xml' }],
-],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['allure-playwright', { outputFolder: 'allure-results', detail: true }],
+  ],
 
   use: {
     headless: true,
