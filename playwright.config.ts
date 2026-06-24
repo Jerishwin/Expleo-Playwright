47ec46457e62e1709c795eed855b2862d48ec461
Jerishwin Joseph <jerishwinj@gmail.com>
2026-06-24 16:14:45 +0530

    24-6-26

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+  const envName = process.env.ENV||'qa';
+  dotenv.config({ path: `./env/.env.${envName}` });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,11 +22,11 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    browserName:'chromium',
+    headless: true,
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video:'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
--- tests/DemoLogin.test.ts
+++ tests/DemoLogin.test.ts
@@ -1,9 +1,9 @@
 import {test , expect} from '@playwright/test';
 
 test('Login Test',async({page})=>{
-    await page.goto('https://the-internet.herokuapp.com/login');
-    await page.fill('#username','tomsmith');
-    await page.fill('#password','SuperSecretPassword!');
+    await page.goto(process.env.BASE_URL!);
+    await page.fill('#username',process.env.UNAME!);
+    await page.fill('#password',process.env.PASSWORD!);
     await page.click('button[type="submit"]');
     await expect(page.locator('.flash.success')).toBeVisible();
 });
